0404dfae9e50f24d5e14be2787a72e3527be7f45
yevgenyl <yevgeny@consist.co.il>
2019-01-03 19:15:39 +0200

    add all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  @ViewChild('home') homeComp: ElementRef;
+
+  onGoToHome() {
+    const homeComp = document.getElementsByTagName('app-home');
+    Array.from(homeComp).forEach((item) => {
+      this.scrollingTo(item);
+    });
+
+  }
+  onGoToAbout() {
+    const aboutComp = document.getElementsByTagName('app-about');
+    Array.from(aboutComp).forEach((item) => {
+      this.scrollingTo(item);
+    });
+  }
+  onGoToContact() {
+    const contactComp = document.getElementsByTagName('app-contact');
+    Array.from(contactComp).forEach((item) => {
+      this.scrollingTo(item);
+    });
+  }
+
+  scrollingTo(item) {
+    const top = item.getBoundingClientRect().top;
+    window.scrollTo({
+      top: top,
+      behavior: 'smooth'
+    });
+  }
 }
